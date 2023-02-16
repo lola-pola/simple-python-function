543c1ad0c991bea0d10001878811b30be8e31c43
Elhay Efrat <elhayefrat@elhays-2.lan>
2023-02-16 09:10:11 +0200

    ddding
    Signed-off-by: Elhay Efrat <elhayefrat@elhays-2.lan>

--- simple-python-function/__init__.py
+++ simple-python-function/__init__.py
@@ -6,6 +6,16 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    # Python 3
+    def mult_numbers(a, b):
+      return a * b
+
+    # Unit test
+    def test_mult_numbers():
+      assert mult_numbers(3, 4) == 12
+      
+    test_mult_numbers()
+    
     name = req.params.get('name')
     if not name:
         try:
