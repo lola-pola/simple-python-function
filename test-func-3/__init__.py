d8975ddb9c31e21e7c1e7084d4f236150b234670
Elhay Efrat <elhayefrat@gmail.com>
2023-05-08 13:14:53 +0300

    dding
    Signed-off-by: Elhay Efrat <elhayefrat@gmail.com>

--- test-func-3/__init__.py
+++ test-func-3/__init__.py
@@ -5,30 +5,93 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    try:
+        import os
+        from azure.identity import DefaultAzureCredential
+        from azure.mgmt.compute import ComputeManagementClient
+        from azure.mgmt.storage import StorageManagementClient
 
-    # Python 3
-    def mult_numbers(a, b):
-      return a * b
 
-    # Unit test
-    def test_mult_numbers():
-      assert mult_numbers(3, 4) == 12
-      
-    test_mult_numbers()
-    
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+        # Set the tag and the maximum number of snapshots to keep per disk
+        tag_name = 'k8s-azure-created-by'
+        tag_value = 'kubernetes-azure-dd'
+        max_snapshots_per_disk = 120
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+        # Set your Azure subscription ID, resource group name, and region
+        # subscription_id = "613ad620-f6ee-4055-bd0a-68a93656bee3"
+        subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
+        # Authenticate with the Azure management API using the default credentials
+        credential = DefaultAzureCredential()
+        compute_client = ComputeManagementClient(credential,subscription_id)
+
+        # Get a list of all disks with the specified tag
+        disks = compute_client.disks.list()
+        # tagged_disks = [d for d in disks if tag_name in d.tags and d.tags[tag_name] == tag_value]
+        tagged_disks = disks
+        for disk in tagged_disks:
+            try:
+            ### create snapshot
+                    snapshot_name = f'{disk.name}-snapshot-1'
+                    print(f"create snapshot {snapshot_name} in resource group {disk.id.split('/')[4]}")
+                    async_snapshot_creation = compute_client.snapshots.begin_create_or_update(
+                    disk.id.split('/')[4], # this is the resource group name
+                    snapshot_name, # this is the snapshot name
+
+                    {
+                        'location': disk.location, # this is the location of the disk
+                        'incremental': 'true', # this enables incremental snapshots
+                        'creation_data': { 
+                            'incremental': 'true', # this enables incremental snapshots
+                            'create_option': 'Copy', 
+                            'source_uri': disk.id # this is the disk ID
+                        }
+                    }
+                    )
+                    try:
+                        snapshot = async_snapshot_creation.result()
+                    except Exception as Error:
+                        pass
+            except Exception as Erorr:
+                pass
+            snapshots = list(compute_client.snapshots.list())
+            snapshots.sort(key=lambda s: s.time_created, reverse=True)
+            for i, snapshot in enumerate(snapshots):
+                if i >= max_snapshots_per_disk:
+                    snap = snapshot.id.split('/')[4]
+                    print(f"deleting snapshot resource group {snap}")
+                    print(f"deleting snapshot {snapshot.name}")
+                    compute_client.snapshots.begin_delete(resource_group_name=snap,snapshot_name=snapshot.name)
+                else:
+                    try:
+                        snapshot_name = f'{disk.name}-snapshot-{i+1}'
+                        print(f"create snapshot {snapshot_name} in resource group {disk.id.split('/')[4]}")
+                        async_snapshot_creation = compute_client.snapshots.begin_create_or_update(
+                        disk.id.split('/')[4],
+                        snapshot_name,
+
+                        {
+                            'location': disk.location,
+                            'incremental': 'true',
+                            'creation_data': {
+                                'incremental': 'true',
+                                'create_option': 'Copy',
+                                'source_uri': disk.id
+                            }
+                        }
+                        )
+
+
+                        snapshot = async_snapshot_creation.result()
+                    except Exception as Error:
+                        pass
+        
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered {Error}",
+             status_code=200
+        )            
+    except Exception as Error:
+        return func.HttpResponse(
+             "This HTTP triggered {Error}",
              status_code=200
         )
+
--- test-func-backup/__init__.py
+++ test-func-backup/__init__.py
@@ -8,29 +8,96 @@ from azure.mgmt.compute import ComputeManagementClient
 
 def main(mytimer: func.TimerRequest, inputBlob: func.InputStream):
     try:
-        logging.info('Python timer trigger function ran at %s', datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'))
+        import os
+        from azure.identity import DefaultAzureCredential
+        from azure.mgmt.compute import ComputeManagementClient
+        from azure.mgmt.storage import StorageManagementClient
 
-        # Load configuration from input blob
-        config = json.load(inputBlob)
 
-        # Authenticate with Azure using default credentials
+        # Set the tag and the maximum number of snapshots to keep per disk
+        tag_name = 'k8s-azure-created-by'
+        tag_value = 'kubernetes-azure-dd'
+        max_snapshots_per_disk = 120
+
+        # Set your Azure subscription ID, resource group name, and region
+        # subscription_id = "613ad620-f6ee-4055-bd0a-68a93656bee3"
+        subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
+        # Authenticate with the Azure management API using the default credentials
         credential = DefaultAzureCredential()
-        # subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
-        subscription_id = "564b567a-e4c8-4f24-b820-1ebf14319aac"
-        compute_client = ComputeManagementClient(credential, subscription_id)
+        compute_client = ComputeManagementClient(credential,subscription_id)
 
-        # Get list of snapshots with specified tags
-        snapshot_list = compute_client.snapshots.list()
-        target_snapshots = []
-        for snapshot in snapshot_list:
-            if all(tag in snapshot.tags for tag in config['tags']):
-                target_snapshots.append(snapshot)
+        # Get a list of all disks with the specified tag
+        disks = compute_client.disks.list()
+        # tagged_disks = [d for d in disks if tag_name in d.tags and d.tags[tag_name] == tag_value]
+        tagged_disks = disks
+        for disk in tagged_disks:
+            try:
+            ### create snapshot
+                    snapshot_name = f'{disk.name}-snapshot-1'
+                    print(f"create snapshot {snapshot_name} in resource group {disk.id.split('/')[4]}")
+                    async_snapshot_creation = compute_client.snapshots.begin_create_or_update(
+                    disk.id.split('/')[4], # this is the resource group name
+                    snapshot_name, # this is the snapshot name
 
-        # Delete any excess snapshots
-        if len(target_snapshots) > config['num_to_keep']:
-            excess_snapshots = sorted(target_snapshots, key=lambda s: s.time_created)[:-(config['num_to_keep'])]
-            for snapshot in excess_snapshots:
-                compute_client.snapshots.begin_delete(snapshot.id)
-                logging.info('Deleted snapshot %s', snapshot.name)
+                    {
+                        'location': disk.location, # this is the location of the disk
+                        'incremental': 'true', # this enables incremental snapshots
+                        'creation_data': { 
+                            'incremental': 'true', # this enables incremental snapshots
+                            'create_option': 'Copy', 
+                            'source_uri': disk.id # this is the disk ID
+                        }
+                    }
+                    )
+                    try:
+                        snapshot = async_snapshot_creation.result()
+                    except Exception as Error:
+                        pass
+            except Exception as Erorr:
+                pass
+            snapshots = list(compute_client.snapshots.list())
+            snapshots.sort(key=lambda s: s.time_created, reverse=True)
+            for i, snapshot in enumerate(snapshots):
+                if i >= max_snapshots_per_disk:
+                    snap = snapshot.id.split('/')[4]
+                    print(f"deleting snapshot resource group {snap}")
+                    print(f"deleting snapshot {snapshot.name}")
+                    compute_client.snapshots.begin_delete(resource_group_name=snap,snapshot_name=snapshot.name)
+                else:
+                    try:
+                        snapshot_name = f'{disk.name}-snapshot-{i+1}'
+                        print(f"create snapshot {snapshot_name} in resource group {disk.id.split('/')[4]}")
+                        async_snapshot_creation = compute_client.snapshots.begin_create_or_update(
+                        disk.id.split('/')[4],
+                        snapshot_name,
+
+                        {
+                            'location': disk.location,
+                            'incremental': 'true',
+                            'creation_data': {
+                                'incremental': 'true',
+                                'create_option': 'Copy',
+                                'source_uri': disk.id
+                            }
+                        }
+                        )
+
+
+                        snapshot = async_snapshot_creation.result()
+                    except Exception as Error:
+                        pass
+        
+        return func.HttpResponse(
+             "This HTTP triggered {Error}",
+             status_code=200
+        )            
     except Exception as Error:
-        print(f'{Error}')
+        return func.HttpResponse(
+             "This HTTP triggered {Error}",
+             status_code=200
+        )
+
+
+
+
+    
